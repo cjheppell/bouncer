0fd74b201ee75480e4970eccb24ca4034f49cd3f
Panagis Artoumas <panagisart@yahoo.com>
2019-11-29 13:22:46 +0000

    Add a port property to the Tcp Service spec.

--- pkg/apis/bouncer/v1alpha1/tcpservice_types.go
+++ pkg/apis/bouncer/v1alpha1/tcpservice_types.go
@@ -4,15 +4,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // TcpServiceSpec defines the desired state of TcpService
 // +k8s:openapi-gen=true
 type TcpServiceSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Port int `json:"port"`
 }
 
 // TcpServiceStatus defines the observed state of TcpService
